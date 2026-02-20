59428b695f350809f4fcf7657ee7f17f67464499
AliPasha <ahmdrzlap327@gmail.com>
2026-02-20 10:32:36 +0700

    ipk baru

--- Minggu2/mahasiswa02.java
+++ Minggu2/mahasiswa02.java
@@ -18,24 +18,23 @@ public class mahasiswa02 {
     }
 
     void updateIpk (double ipkBaru) {
-        ipk = ipkBaru;
+        if (ipkBaru >= 0.0 && ipkBaru <= 4.0) {
+            ipk = ipkBaru;
+        } else {
+            System.out.println("IPK tidak valid. Harus antara 0.0 dan 4.0"); 
+        }
     }
 
     String nilaiKinerja () {
-        if (ipk > 0 || ipk < 4) {
-            return "IPK tidak valid. Harus antara 0.0 dan 4.0";
+        if (ipk >= 3.5) {
+            return "Kinerja Sangat Baik";
+        } else if (ipk >= 3.0) {
+            return "Kinerja Baik";
+        } else if (ipk >= 2.5) {
+            return "Kinerja Cukup";
         } else {
-            if (ipk >= 3.5) {
-                return "Kinerja Sangat Baik";
-            } else if (ipk >= 3.0) {
-                return "Kinerja Baik";
-            } else if (ipk >= 2.5) {
-                return "Kinerja Cukup";
-            } else {
-                return "Kinerja Kurang";
-            }
+            return "Kinerja Kurang";
         }
-        
     }
     
 }
