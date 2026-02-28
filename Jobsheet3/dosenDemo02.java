1686fdec0c7fb75edc48dd33f766f543810575c2
AliPasha <ahmdrzlap327@gmail.com>
2026-02-28 10:18:56 +0700

    tugas 1

---
+++ Jobsheet3/dosenDemo02.java
@@ -0,0 +1,43 @@
+package Jobsheet3;
+import java.util.Scanner;
+public class dosenDemo02 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Masukkan Jumlah Dosen: ");
+        int jumlahDosen = sc.nextInt();
+        sc.nextLine();
+
+        dosen02[] arrayOfDosen = new dosen02[jumlahDosen];
+        for (int i = 0; i < arrayOfDosen.length; i++) {
+            System.out.println("Masukkan Data Dosen ke-" + (i + 1));
+            System.out.print("Kode          : ");
+            String kode = sc.nextLine();
+            System.out.print("Nama          : ");
+            String nama = sc.nextLine();
+            System.out.print("Jenis Kelamin (P/W): ");
+            char jkInput = sc.next().charAt(0);
+            boolean jk = (jkInput == 'p' || jkInput == 'P');
+            System.out.print("Usia          : ");
+            int usia = sc.nextInt();
+            sc.nextLine(); 
+            System.out.println("--------------------------------");
+
+            // Instansiasi objek langsung dengan constructor berparameter
+            arrayOfDosen[i] = new dosen02(kode, nama, jk, usia);
+        }
+
+        System.out.println("\n======= DAFTAR DOSEN =======");
+        int counter = 1;
+        for (dosen02 dosen : arrayOfDosen) {
+            System.out.println("Data Dosen ke-" + counter);
+            System.out.println("Kode          : " + dosen.kode);
+            System.out.println("Nama          : " + dosen.nama);
+            // Konversi Boolean ke teks agar sesuai contoh output
+            String gender = dosen.jenisKelamin ? "Pria" : "Wanita";
+            System.out.println("Jenis Kelamin : " + gender);
+            System.out.println("Usia          : " + dosen.usia);
+            System.out.println("--------------------------------");
+            counter++;
+        }
+    }
+}
