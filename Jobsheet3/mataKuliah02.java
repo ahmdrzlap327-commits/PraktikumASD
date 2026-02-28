1321c2a5add115e99536c1b0fc8f5ad40f7abe43
AliPasha <ahmdrzlap327@gmail.com>
2026-02-28 09:45:17 +0700

    .

---
+++ Jobsheet3/mataKuliah02.java
@@ -0,0 +1,18 @@
+package Jobsheet3;
+
+import java.util.Scanner;
+
+public class mataKuliah02 {
+    public String kode;
+    public String nama;
+    public int sks;
+    public int jumlahJam;
+
+    public mataKuliah02(String kode, String nama, int sks, int jumlahJam) {
+        this.kode = kode;
+        this.nama = nama;
+        this.sks = sks;
+        this.jumlahJam = jumlahJam;
+    }
+
+}
