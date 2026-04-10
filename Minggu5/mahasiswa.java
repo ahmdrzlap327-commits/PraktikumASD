5a3fe0fb3e7ce3ba597f3a32ee2b06f0ee2d8e3c
AliPasha <ahmdrzlap327@gmail.com>
2026-04-10 19:49:56 +0700

    4

---
+++ Minggu5/mahasiswa.java
@@ -0,0 +1,42 @@
+package Minggu5;
+
+public class mahasiswa {
+    String nama;
+    int nim, tahunMasuk, nilaiUTS, nilaiUAS;
+
+    mahasiswa(String nm, int nim, int thn, int uts, int uas){
+        nama = nm;
+        this.nim = nim;
+        tahunMasuk = thn;
+        nilaiUTS = uts;
+        nilaiUAS = uas;
+    }
+
+    int cariMaxUTS(mahasiswa[] mhs, int l, int r) {
+        if (l == r) {
+            return mhs[l].nilaiUTS; 
+        }
+        int mid = (l + r) / 2; 
+        int maxL = cariMaxUTS(mhs, l, mid); 
+        int maxR = cariMaxUTS(mhs, mid + 1, r); 
+        return (maxL > maxR) ? maxL : maxR; 
+    }
+
+    int cariMinUTS(mahasiswa[] mhs, int l, int r) {
+        if (l == r) {
+            return mhs[l].nilaiUTS;
+        }
+        int mid = (l + r) / 2;
+        int minL = cariMinUTS(mhs, l, mid);
+        int minR = cariMinUTS(mhs, mid + 1, r);
+        return (minL < minR) ? minL : minR;
+    }
+
+    double rataUAS(mahasiswa[] mhs) {
+        double total = 0;
+        for (int i = 0; i < mhs.length; i++) {
+            total += mhs[i].nilaiUAS; // Iteratif Brute Force [cite: 164, 165]
+        }
+        return total / mhs.length;
+    }
+}
---
+++ Minggu5/mainMahasiswa.java
@@ -0,0 +1,23 @@
+package Minggu5;
+
+public class mainMahasiswa {
+    public static void main(String[] args) {
+        mahasiswa[] daftar = {
+            new mahasiswa("Ahmad", 220101001, 2022, 78, 82),
+            new mahasiswa("Budi", 220101002, 2022, 85, 88),
+            new mahasiswa("Cindy", 220101003, 2021, 90, 87),
+            new mahasiswa("Dian", 220101004, 2021, 76, 79),
+            new mahasiswa("Eko", 220101005, 2023, 92, 95),
+            new mahasiswa("Fajar", 220101006, 2020, 88, 85),
+            new mahasiswa("Gina", 220101007, 2023, 80, 83),
+            new mahasiswa("Hadi", 220101008, 2020, 82, 84)
+        };
+
+        mahasiswa helper = daftar[0];
+        System.out.println("=== HASIL LATIHAN PRAKTIKUM ===");
+        System.out.println("UTS Tertinggi (DC): " + helper.cariMaxUTS(daftar, 0, daftar.length - 1));
+        System.out.println("UTS Terendah  (DC): " + helper.cariMinUTS(daftar, 0, daftar.length - 1));
+        System.out.println("Rata-rata UAS (BF): " + helper.rataUAS(daftar));
+
+    }
+}
