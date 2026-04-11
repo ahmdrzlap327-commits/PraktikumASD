710bc92b6935ee380542f1d22abb831ee8ca6053
AliPasha <ahmdrzlap327@gmail.com>
2026-04-11 16:17:34 +0700

    7

--- Jobsheet5/sortingMain02.java
+++ Jobsheet5/sortingMain02.java
@@ -4,6 +4,7 @@ public class sortingMain02 {
     public static void main(String[] args) {
         int a [] = {20, 10, 2, 7, 12};
         int b [] = {30, 20, 2, 8, 14};
+        int c [] = {40, 10, 4, 9, 3};
 
         sorting02 dataurut1 = new sorting02(a, a.length);
 
@@ -13,11 +14,18 @@ public class sortingMain02 {
         System.out.println("Data sudah diurutkan dengan BUBBLE SHORT (ASC)");
         dataurut1.tampil();
 
-        sorting02 dataurut2 = new sorting02(a,  b.length);
+        sorting02 dataurut2 = new sorting02(b,  b.length);
         System.out.println("Data awal 2");
         dataurut2.tampil();
         dataurut2.selectionsort();
         System.out.println("Data sudah diurutkan dengan SELECTION SORT (ASC)");
         dataurut2.tampil();
+
+        sorting02 dataurut3 = new sorting02(c,  c.length);
+        System.out.println("Data awal 3");
+        dataurut3.tampil();
+        dataurut3.selectionsort();
+        System.out.println("Data sudah diurutkan dengan INSERTION SORT (ASC)");
+        dataurut3.tampil();
     }
 }
