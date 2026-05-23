f9f3fc6e99003cb635f8cb1fbe55ad06e9d0fad3
AliPasha <ahmdrzlap327@gmail.com>
2026-05-23 20:56:56 +0700

    praktikum2

--- Jobsheet11/SLLmain02.java
+++ Jobsheet11/SLLmain02.java
@@ -32,18 +32,31 @@ public class SLLmain02 {
         System.out.println("\n=================================");
         sll.print();
 
-        // mahasiswa02 mhs1= new mahasiswa02("24212200", "Alvaro", "1A", 4.0);
-        // mahasiswa02 mhs2 = new mahasiswa02("23212201", "Bimon", "2B", 3.8);
-        // mahasiswa02 mhs3 = new mahasiswa02("22212202", "Cintia", "3C", 3.5);
-        // mahasiswa02 mhs4 = new mahasiswa02("21212203", "Dirga", "4D", 3.6);
+        mahasiswa02 mhs1= new mahasiswa02("24212200", "Alvaro", "1A", 4.0);
+        mahasiswa02 mhs2 = new mahasiswa02("23212201", "Bimon", "2B", 3.8);
+        mahasiswa02 mhs3 = new mahasiswa02("22212202", "Cintia", "3C", 3.5);
+        mahasiswa02 mhs4 = new mahasiswa02("21212203", "Dirga", "4D", 3.6);
 
-        // sll.print();
-        // sll.addFirst(mhs4);
-        // sll.print();
-        // sll.addLast(mhs1);
-        // sll.print();
-        // sll.insertAfter("Dirga", mhs3);
-        // sll.insertAt(2, mhs2);
-        // sll.print();
+        sll.print();
+        sll.addFirst(mhs4);
+        sll.print();
+        sll.addLast(mhs1);
+        sll.print();
+        sll.insertAfter("Dirga", mhs3);
+        sll.insertAt(2, mhs2);
+        sll.print();
+
+        
+        System.out.println("data index 1 : ");
+        sll.getData(1);
+
+        System.out.println("data mahasiswa an Bimon berada pada index : " + sll.indexOf("bimon"));
+        System.out.println();
+
+        sll.removeFirst();
+        sll.removeLast();
+        sll.print();
+        sll.removeAt(0);
+        sll.print();
     }
 }
