339698401c4380cc2b553e16083816c24cede283
AliPasha <ahmdrzlap327@gmail.com>
2026-05-23 20:03:22 +0700

    modifikasi pertanyaan

--- Jobsheet11/SLLmain02.java
+++ Jobsheet11/SLLmain02.java
@@ -5,18 +5,45 @@ public class SLLmain02 {
         Scanner sc = new Scanner(System.in);
         singleLinkedList02 sll = new singleLinkedList02();
 
-        mahasiswa02 mhs1= new mahasiswa02("24212200", "Alvaro", "1A", 4.0);
-        mahasiswa02 mhs2 = new mahasiswa02("23212201", "Bimon", "2B", 3.8);
-        mahasiswa02 mhs3 = new mahasiswa02("22212202", "Cintia", "3C", 3.5);
-        mahasiswa02 mhs4 = new mahasiswa02("21212203", "Dirga", "4D", 3.6);
+        System.out.println("=== INPUT DATA MAHASISWA (LINKED LIST) ===");
+        System.out.print("Masukkan Jumlah Mahasiswa yang ingin diinput: ");
+        int jumlah = sc.nextInt();
+        sc.nextLine();
 
+        for (int i = 0; i < jumlah; i++) {
+            System.out.println("\nData Mahasiswa ke-" + (i + 1));
+            System.out.print("NIM   : "); 
+            String nim = sc.nextLine();
+            System.out.print("Nama  : "); 
+            String nama = sc.nextLine();
+            System.out.print("Kelas : "); 
+            String kelas = sc.nextLine();
+            System.out.print("IPK   : "); 
+            double ipk = sc.nextDouble();
+            sc.nextLine(); 
+
+            // Buat objek mahasiswa berdasarkan input keyboard
+            mahasiswa02 mhsBaru = new mahasiswa02(nim, nama, kelas, ipk);
+
+            // Contoh otomatis ditambahkan ke urutan paling belakang (addLast)
+            sll.addLast(mhsBaru);
+        }
+
+        System.out.println("\n=================================");
         sll.print();
-        sll.addFirst(mhs4);
-        sll.print();
-        sll.addLast(mhs1);
-        sll.print();
-        sll.insertAfter("Dirga", mhs3);
-        sll.insertAt(2, mhs2);
-        sll.print();
+
+        // mahasiswa02 mhs1= new mahasiswa02("24212200", "Alvaro", "1A", 4.0);
+        // mahasiswa02 mhs2 = new mahasiswa02("23212201", "Bimon", "2B", 3.8);
+        // mahasiswa02 mhs3 = new mahasiswa02("22212202", "Cintia", "3C", 3.5);
+        // mahasiswa02 mhs4 = new mahasiswa02("21212203", "Dirga", "4D", 3.6);
+
+        // sll.print();
+        // sll.addFirst(mhs4);
+        // sll.print();
+        // sll.addLast(mhs1);
+        // sll.print();
+        // sll.insertAfter("Dirga", mhs3);
+        // sll.insertAt(2, mhs2);
+        // sll.print();
     }
 }
